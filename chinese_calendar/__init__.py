0860724e4b562cf7cce37bce9e9f3f4520d2523f
eeeqzq <eeeqzq@qq.com>
2025-02-23 16:37:49 +0800

    Update __init__.py

--- chinese_calendar/__init__.py
+++ chinese_calendar/__init__.py
@@ -14,6 +14,14 @@ from .utils import (
     is_workday,
 )
 
+from .utils2 import (
+    get_workweek_num, 
+    get_workweek_range, 
+    get_t_range, 
+    next_day, 
+    T_n,
+    )
+
 __version__ = "1.10.0"
 __all__ = [
     "Holiday",
@@ -29,4 +37,9 @@ __all__ = [
     "get_holidays",
     "get_workdays",
     "find_workday",
+    "get_workweek_num",
+    "get_workweek_range",
+    "next_day",
+    "get_t_range",
+    "T_n",
 ]
